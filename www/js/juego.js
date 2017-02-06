13b339368d45769b3461d04047b28879a3c2c331
Cavaliero <francarrero_5@hotmail.com>
2017-02-06 02:51:19 +0100

    añadida funcionalidad

--- www/js/juego.js
+++ www/js/juego.js
@@ -5,6 +5,7 @@ var app={
     velocidadX = 0;
     velocidadY = 0;
     puntuacion = 0;
+    colorFondo = '#f27d0c';
     
     alto  = document.documentElement.clientHeight;
     ancho = document.documentElement.clientWidth;
@@ -18,7 +19,7 @@ var app={
     function preload() {
       game.physics.startSystem(Phaser.Physics.ARCADE);
 
-      game.stage.backgroundColor = '#f27d0c';
+      game.stage.backgroundColor = colorFondo;
       game.load.image('bola', 'assets/bola.png');
       game.load.image('objetivo', 'assets/objetivo.png');
       game.load.image('objetivo2', 'assets/objetivo2.png');
@@ -47,20 +48,24 @@ var app={
       
       game.physics.arcade.overlap(bola, objetivo, app.incrementaPuntuacion, null, this);
       game.physics.arcade.overlap(bola, objetivo2, app.incrementaPuntuacionPlus, null, this);
+
+
+//ESTO PETAAAAAAAA
+      if(bola.body.checkWorldBounds()===false){
+      game.stage.backgroundColor=colorFondo;
+      }else{
+      game.stage.backgroundColor='#ff3300';
+      }
     }
 
     var estados = { preload: preload, create: create, update: update };
     var game = new Phaser.Game(ancho, alto, Phaser.CANVAS, 'phaser',estados);
   },
 
-
-  cambiaFondo: function(){
-    game.stage.backgroundColor = '#800909';
-  },
-
   decrementaPuntuacion: function(){
     puntuacion = puntuacion-1;
     scoreText.text = puntuacion;
+    app.oscureceFondo();
   },
 
   incrementaPuntuacion: function(){
@@ -72,6 +77,7 @@ var app={
 
     if (puntuacion > 0){
       dificultad = dificultad + 1;
+      app.aclaraFondo();
     }
   },
 
@@ -84,7 +90,56 @@ var app={
 
     if (puntuacion > 0){
       dificultad = dificultad + 1;
+      app.aclaraFondo();
     }
+  },
+
+    aclaraFondo: function(){
+    CompRed = '0x'+colorFondo[1]+colorFondo[2];
+    CompGreen = '0x'+colorFondo[3]+colorFondo[4];
+    CompBlue = '0x'+colorFondo[5]+colorFondo[6];
+
+    CompRed = parseInt(CompRed, 16);
+    CompGreen = parseInt(CompGreen, 16);
+    CompBlue = parseInt(CompBlue, 16);
+
+    CompRed = CompRed+0x10;
+    CompGreen = CompGreen+0x10;
+    CompBlue = CompBlue+0x10;
+
+    if(CompRed>=0xFF){CompRed=0xFF};
+    if(CompGreen>=0xFF){CompGreen=0xFF};
+    if(CompBlue>=0xFF){CompBlue=0xFF};
+
+    CompRed = CompRed.toString(16);
+    CompGreen = CompGreen.toString(16);
+    CompBlue = CompBlue.toString(16);    
+
+    colorFondo = '#' + CompRed + CompGreen + CompBlue;
+  },
+
+  oscureceFondo: function(){
+    CompRed = '0x'+colorFondo[1]+colorFondo[2];
+    CompGreen = '0x'+colorFondo[3]+colorFondo[4];
+    CompBlue = '0x'+colorFondo[5]+colorFondo[6];
+
+    CompRed = parseInt(CompRed, 16);
+    CompGreen = parseInt(CompGreen, 16);
+    CompBlue = parseInt(CompBlue, 16);
+
+    CompRed = CompRed-0x10;
+    CompGreen = CompGreen-0x10;
+    CompBlue = CompBlue-0x10;
+
+    if(CompRed<=0xf2){CompRed=0xf2};
+    if(CompGreen<=0x7d){CompGreen=0x7d};
+    if(CompBlue<=0x0c){CompBlue='0c'};
+
+    CompRed = CompRed.toString(16);
+    CompGreen = CompGreen.toString(16);
+    CompBlue = CompBlue.toString(16);    
+
+    colorFondo = '#' + CompRed + CompGreen + CompBlue;
   },
 
   inicioX: function(){
